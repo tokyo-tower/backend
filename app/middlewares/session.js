701a93f9be4d47388132c8338dda6fb8f511aba5
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2017-12-18 04:08:22 +0000

    ### Changed - セッションクライアントを調整。

--- app/middlewares/session.js
+++ app/middlewares/session.js
@@ -11,14 +11,14 @@ const redis = require("redis");
 const redisStore = connectRedis(session);
 const COOKIE_MAX_AGE = 3600000; // 60 * 60 * 1000(session active 1 hour)
 exports.default = session({
-    secret: 'ttts-backend-secret',
+    secret: 'ttts-backend-session-secret',
     resave: false,
     // Force a session identifier cookie to be set on every response.
     // The expiration is reset to the original maxAge, resetting the expiration countdown.
     rolling: true,
     saveUninitialized: false,
     store: new redisStore({
-        client: redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {
+        client: redis.createClient(Number(process.env.REDIS_PORT), process.env.REDIS_HOST, {
             password: process.env.REDIS_KEY,
             tls: { servername: process.env.REDIS_HOST },
             return_buffers: true
